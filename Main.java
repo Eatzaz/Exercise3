cbd8316d6681c21650bb3a75f6dd5f8071c874b4
Eatzaz <126365314+Eatzaz@users.noreply.github.com>
2025-02-22 22:47:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,12 +33,13 @@ public class Main {
 
         //3.Write a program to find the factorial value of any number entered
         //through the keyboard.
-        int num = 9;
-        int f = 0;
-        for (int i = 0; i <= num; i++) {
-            f = num * (i + 1);
+        System.out.println("enter number:");
+        int num= input.nextInt();
+        int f=1;
+        for(int i=1;i<=num;i++){
+        f=f*i;
         }
-        System.out.println("The factorial value is:" + f);
+        System.out.println(f);
 
         //4.Two numbers are entered through the keyboard. Write a program to find
         // the value of one number raised to the power of another. (Do not use Java
